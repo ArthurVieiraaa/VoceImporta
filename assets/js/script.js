474ddc9e00cdb3caea238fb0066a0747bd8d0cf0
Arthur Vieira Bruske <avieirabruske@gmail.com>
2024-06-08 20:20:13 -0300

    Finalizando Front-End Index

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,8 @@ setTimeout(function() {
     elem_loading.classList.remove("loading");
     }, 1280);
 
-const openModalButton = document.querySelector("#open-modal");
-const closeModalButton = document.querySelector("#close-modal");
+const openModalCadastroButton = document.querySelector("#open-modal-cadastro");
+const closeModalCadastroButton = document.querySelector("#close-modal-cadastro");
 const modal = document.querySelector("#modal");
 const fade = document.querySelector("#fade");
 
@@ -16,9 +16,26 @@ const toggleModal = () => {
     [modal, fade].forEach((el) => el.classList.toggle("hide"));
 }
 
-[openModalButton, closeModalButton, fade].forEach((el) => {
+[openModalCadastroButton, closeModalCadastroButton, fade].forEach((el) => {
     el.addEventListener("click", () => toggleModal());{
         modal.classList.toggle("hidden");
         fade.classList.toggle("hidden");
     };
-});
+})
+
+const openModalEntrarButton = document.querySelector("#open-modal-entrar");
+const closeModalEntrarButton = document.querySelector("#close-modal-entrar");
+const modalEntrar = document.querySelector("#modalEntrar");
+const fadeEntrar = document.querySelector("#fadeEntrar");
+
+const toggleModalEntrar = () => {
+    [modalEntrar, fadeEntrar].forEach((el) => el.classList.toggle("hide"));
+}
+
+[openModalEntrarButton, closeModalEntrarButton, fadeEntrar].forEach((el) => {
+    el.addEventListener("click", () => toggleModalEntrar());{
+        modalEntrar.classList.toggle("hidden")
+        fadeEntrar.classList.toggle("hidden")
+    };
+})
+
