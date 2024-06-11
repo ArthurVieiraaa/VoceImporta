26ea26fabb11109270583ac41265e21a766f6681
Arthur Vieira Bruske <avieirabruske@gmail.com>
2024-06-11 15:43:27 -0300

    Criando Novo Layout Index

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,35 +7,7 @@ setTimeout(function() {
     elem_loading.classList.remove("loading");
     }, 1280);
 
-const openModalCadastroButton = document.querySelector("#open-modal-cadastro");
-const closeModalCadastroButton = document.querySelector("#close-modal-cadastro");
-const modal = document.querySelector("#modal");
-const fade = document.querySelector("#fade");
-
-const toggleModal = () => {
-    [modal, fade].forEach((el) => el.classList.toggle("hide"));
-}
-
-[openModalCadastroButton, closeModalCadastroButton, fade].forEach((el) => {
-    el.addEventListener("click", () => toggleModal());{
-        modal.classList.toggle("hidden");
-        fade.classList.toggle("hidden");
-    };
+window.addEventListener("scroll", function(){
+    let header = document.querySelector('#header')
+    header.classList.toggle('rolagem', window.scrollY > 250)
 })
-
-const openModalEntrarButton = document.querySelector("#open-modal-entrar");
-const closeModalEntrarButton = document.querySelector("#close-modal-entrar");
-const modalEntrar = document.querySelector("#modalEntrar");
-const fadeEntrar = document.querySelector("#fadeEntrar");
-
-const toggleModalEntrar = () => {
-    [modalEntrar, fadeEntrar].forEach((el) => el.classList.toggle("hide"));
-}
-
-[openModalEntrarButton, closeModalEntrarButton, fadeEntrar].forEach((el) => {
-    el.addEventListener("click", () => toggleModalEntrar());{
-        modalEntrar.classList.toggle("hidden")
-        fadeEntrar.classList.toggle("hidden")
-    };
-})
-
