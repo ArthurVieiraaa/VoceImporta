077c5ca3fbaf7d0732ca8af1c1dad363a448de5d
Arthur Vieira Bruske <avieirabruske@gmail.com>
2024-06-12 16:57:58 -0300

    Atualizando Index V2.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,6 @@
-let elem_preloading = document.getElementById("preloading");
-let elem_loading = document.getElementById("loading");
-console.log(" Ok");
-
-setTimeout(function() {
-    elem_preloading.classList.remove("preloading");
-    elem_loading.classList.remove("loading");
-    }, 1280);
-
 window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
     header.classList.toggle('rolagem', window.scrollY > 250)
 })
+
+AOS.init();
