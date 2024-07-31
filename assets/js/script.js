8254c173afea408cd450deb7a91a80537e74100b
Arthur Vieira Bruske <avieirabruske@gmail.com>
2024-07-30 21:15:11 -0300

    Atualização

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,12 @@ window.addEventListener("scroll", function(){
 })
 
 AOS.init();
+
+let elem_preloading = document.getElementById("preloading");
+let elem_loading = document.getElementById("loading");
+console.log(" Ok");
+
+setTimeout(function() {
+    elem_preloading.classList.remove("preloading");
+    elem_loading.classList.remove("loading");
+    }, 1280);
