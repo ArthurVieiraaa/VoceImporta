5f4061e7e0970f21dddd52be5fc12594cd1ec939
Arthur Vieira Bruske <avieirabruske@gmail.com>
2024-09-17 03:46:30 -0300

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
-    header.classList.toggle('rolagem', window.scrollY > 250)
+    header.classList.toggle('rolagem', window.scrollY > 100)
 })
 
 AOS.init();
